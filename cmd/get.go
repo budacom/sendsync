f0f3fceb7fa6e1ef369654cb7c10d8923f61bcc2
mauricepoirrier <mauricepoirrier@gmail.com>
2021-05-11 15:11:19 +0000

    refactor: remove unnecesary code from get

--- cmd/get.go
+++ cmd/get.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2021 NAME HERE <EMAIL ADDRESS>
+Copyright © 2021 Buda.com
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -17,37 +17,21 @@ package cmd
 
 import (
 	"fmt"
-	"strings"
 
 	"github.com/spf13/cobra"
 )
 
-// getCmd represents the get command
 var getCmd = &cobra.Command{
 	Use:   "get",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Retrieve from Sendgrid",
+	Long: `Currently only used to fetch templates using:
+	
+sendsync get template`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("get called")
-		fmt.Println("Here are the arguments of card command : " + strings.Join(args, ","))
+		fmt.Println("Nothing here. Run sendsync get template -h")
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(getCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// getCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// getCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
